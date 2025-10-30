5b41706023430b2fe01e615fd0264794c047e6fd
Qaddis <svyatoslavb1107@gmail.com>
2025-10-31 00:13:11 +0300

    Add styles; configure metadata

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,9 @@
+import type { Metadata } from "next"
+
+export const metadata: Metadata = {
+	title: "Главная | Тестовое задание от Барсукова С.Д."
+}
+
 export default function Home() {
 	return <h1>Hello world!</h1>
 }
