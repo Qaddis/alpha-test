ea32fbd67f9bb362d9cc8b4b369a7fdc63532546
Qaddis <svyatoslavb1107@gmail.com>
2025-11-01 10:34:45 +0300

    Add AppHeader component

---
+++ src/components/layout/AppHeader.tsx
@@ -0,0 +1,24 @@
+"use client"
+
+import Link from "next/link"
+
+import HeaderLink from "../ui/HeaderLink"
+
+import styles from "./AppHeader.module.css"
+
+export default function AppHeader() {
+	return (
+		<header className={styles.header}>
+			<div className={styles.wrapper}>
+				<h1 className={styles.logo}>
+					<Link href="/">Test Market</Link>
+				</h1>
+
+				<nav className={styles.nav}>
+					<HeaderLink to="/">Главная</HeaderLink>
+					<HeaderLink to="/product">Продукты</HeaderLink>
+				</nav>
+			</div>
+		</header>
+	)
+}
