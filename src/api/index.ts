5ef758a6018988fad83b703185280b7543c260e2
Qaddis <svyatoslavb1107@gmail.com>
2025-11-01 15:04:27 +0300

    Add a request to the API with typing

---
+++ src/api/index.ts
@@ -0,0 +1,20 @@
+import type { IProduct } from "@/types/product.types"
+
+/**
+ * Возвращает список продуктов, полученных со стороннего API (DummyJSON)
+ * @returns {IProduct[]} Возвращает массив продуктов
+ */
+export const getData = async (): Promise<IProduct[]> => {
+	const response = await fetch("https://dummyjson.com/products")
+	const data = await response.json()
+
+	return data.products.map((item: IProduct) => ({
+		id: item.id,
+		title: item.title,
+		description: item.description,
+		price: item.price,
+		discount: item.discount,
+		rating: item.rating,
+		images: item.images
+	}))
+}
---
+++ src/types/product.types.ts
@@ -0,0 +1,9 @@
+export interface IProduct {
+	id: number
+	title: string
+	description: string
+	price: number
+	discount: number
+	rating: number
+	images: string[]
+}
