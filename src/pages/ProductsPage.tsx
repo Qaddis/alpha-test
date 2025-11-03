93669ed0be369df412080145fa6e13237f840fa2
Qaddis <svyatoslavb1107@gmail.com>
2025-11-03 21:56:36 +0300

    Add product display

--- src/pages/ProductsPage.tsx
+++ src/pages/ProductsPage.tsx
@@ -1,13 +1,33 @@
 "use client"
 
-import PageHeading from "@/components/ui/PageHeading"
+import { useEffect } from "react"
 
+import PageHeading from "@/components/ui/PageHeading"
+import useProducts from "@/stores/productStore"
+
+import ProductCard from "@/components/features/ProductCard"
 import styles from "./ProductsPage.module.css"
 
 export default function ProductsPage() {
+	const { products, init } = useProducts()
+
+	useEffect(() => {
+		if (products.length === 0) init()
+	}, [products, init])
+
 	return (
 		<section className={styles.page}>
 			<PageHeading>Продукты</PageHeading>
+
+			<div className={styles["cards-container"]}>
+				{products.length === 0 ? (
+					<span>Пусто ;(</span>
+				) : (
+					products.map(product => (
+						<ProductCard key={`product-${product.id}`} {...product} />
+					))
+				)}
+			</div>
 		</section>
 	)
 }
