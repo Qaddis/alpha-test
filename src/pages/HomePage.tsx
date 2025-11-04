fb20cb1691cc732f6e553fb7e03e5cb417dc121d
Qaddis <svyatoslavb1107@gmail.com>
2025-11-04 13:51:47 +0300

    Add display of favorite products on the HomePage

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,13 +1,40 @@
 "use client"
 
+import Link from "next/link"
+
+import ProductCard from "@/components/features/ProductCard"
+import ProductsContainer from "@/components/layout/ProductsContainer"
 import PageHeading from "@/components/ui/PageHeading"
+import useProducts from "@/stores/productStore"
 
 import styles from "./HomePage.module.css"
 
 export default function HomePage() {
+	const favoriteProducts = useProducts(state => state.products).filter(
+		product => product.isFavorite
+	)
+
 	return (
-		<section className={styles.page}>
+		<section>
 			<PageHeading>Главная</PageHeading>
+
+			<div>
+				<h3 className={styles.favorites__heading}>Избранное</h3>
+
+				{favoriteProducts.length === 0 ? (
+					<div className={styles["no-favorites"]}>
+						<h4>Пока что тут пусто...</h4>
+
+						<Link href="/products">К продуктам</Link>
+					</div>
+				) : (
+					<ProductsContainer>
+						{favoriteProducts.map(product => (
+							<ProductCard key={`product-${product.id}`} {...product} />
+						))}
+					</ProductsContainer>
+				)}
+			</div>
 		</section>
 	)
 }
